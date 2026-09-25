36a736938341d59eebe0f61e33a37b4ce9df3879
eebyak <berkling.kay@gmail.com>
2026-09-25 13:29:15 +0200

    Add initial Astro website and GitHub Pages deployment

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // DYNAMOS is a GitHub project site:
+  // https://<username>.github.io/DYNAMOS/
+  base: '/DYNAMOS',
+});
